9c52c8c232486f02f87be9ef282148f490594751
AdipVhack356 <64851330+AdipVhack356@users.noreply.github.com>
2020-06-02 22:42:48 +0700

    Create Install.py

---
+++ Install.py
@@ -0,0 +1,96 @@
+#!/usr/bin/python2
+#jangan di recode
+#recode tidak mmenjadikan mu pembuat
+
+import os , sys , time
+
+os.system('clear')
+print('Subscribe YT Gua Dulu Geng')
+time.sleep(1.5)
+os.system('xdg-open https://www.Youtube.com/channel/UCqMYXvvp-DJ2ORRNioZoM3w')
+
+os.system('clear')
+
+print 'Loading.......'
+time.sleep(5)
+os.system('clear')
+print '\t===================='
+print '\t|Author:Adip|'
+print '\t|YESoutube:Adip Official|'
+print '\t|Github:-|'
+print '\t===================='
+print ''
+print 'Tools Ini HanYA Menginstall Berbagai Program Berat'
+time.sleep(4)
+print ''
+print 'Tunggu 10Detik'
+time.sleep(10)
+print ''
+print 'Bersihkan Chat'
+time.sleep(3)
+print ''
+print 'Sedang Di Bersihkan'
+time.sleep(4)
+print ''
+print ('Terbersihkan [YES]')
+time.sleep(2)
+print ''
+os.system('clear')
+print 'Loading.....'
+time.sleep(5)
+
+os.system('clear')
+
+print 'Menginstall.......'
+time.sleep(2)
+os.system('pkg update')
+print 'Install Selesai[YES]'
+time.sleep(3)
+os.system('clear')
+
+print 'Menginstall.......'
+time.sleep(2)
+os.system('pkg upgrade')
+print 'Install Selesai[YES]'
+time.sleep(3)
+os.system('clear')
+
+print 'Menginstall.......'
+time.sleep(2)
+os.system('pkg install python')
+print 'Install Selesai[YES]'
+time.sleep(3)
+os.system('clear')
+
+print 'Menginstall.......'
+time.sleep(2)
+os.system('pkg install figlet')
+print 'Install Selesai[YES]'
+time.sleep(3)
+os.system('clear')
+
+print 'Menginstall.......'
+time.sleep(2)
+os.system('pkg install nano')
+print 'Install Selesai[YES]'
+time.sleep(3)
+os.system('clear')
+
+print 'Menginstall.......'
+time.sleep(2)
+os.system('pkg install git')
+print 'Install Selesai[YES]'
+time.sleep(3)
+os.system('clear')
+
+os.system('xdg-open https://www.Youtube.com/channel/UCqMYXvvp-DJ2ORRNioZoM3w')
+os.system('clear')
+print '\tThanks Bagi YESang Udh Subscribe'
+os.system('clear')
+print('\t<<<<<Thanks Use Tools>>>>>')
+print 'Pass Nya 1'
+pilih = input('Exit:')
+if pilih == 1:
+	print 'Sampai Jumpa Di Tools Selanjut NYA'
+	time.sleep(3)
+	os.system('clear')
